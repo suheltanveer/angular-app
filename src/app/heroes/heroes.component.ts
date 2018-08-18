49ae3a4645564b5448b722a1da77c28d1d26f728
Suhel Tanveer <suhell@Suhels-MacBook-Air.local>
2018-08-18 11:45:03 +0530

    intermediate changes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,6 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-    selectedHero: Hero;
     heroes: Hero[];
 
     constructor(
@@ -18,10 +17,6 @@ export class HeroesComponent implements OnInit {
 
     ngOnInit() {
         this.getHeroes();
-    }
-
-    onSelect(hero: Hero) {
-        this.selectedHero = hero;
     }
 
     getHeroes(): void {
